8d78a66e9f140d2d46809eb6b056ccb73d9007d2
zjk955 <2624221157@qq.com>
2019-06-09 17:08:38 +0800

    修改

--- src/test/java/com/tensquare/test/MongoTest.java
+++ src/test/java/com/tensquare/test/MongoTest.java
@@ -25,23 +25,22 @@ public class MongoTest {
         //封装查询条件只查询用户id为1013的
         //BasicDBObject bson = new BasicDBObject("userid","1013");
         //封装条件，查询访问量大于1000的 find（{visits:{$gt:1000}}）
-        BasicDBObject bson = new BasicDBObject("visits",new BasicDBObject("$gt",1000));
+        BasicDBObject bson = new BasicDBObject("visits", new BasicDBObject("$gt", 1000));
         //得到集合中所有的文档
 //        FindIterable<Document> documents = spit.find(bson);
         //遍历数据
 //        for (Document document : documents ) {
 //            System.out.println("内容:"+document.getString("content"));
 //            System.out.println("用户ID:"+document.getString("userid"));
-//            System.out.println("访问量:"+document.getInteger("visits"));
+ //           System.out.println("访问量:"+document.getInteger("visits"));
 //        }
 
 
-
         //添加记录
-        Map<String , Object> map = new HashMap<String , Object>();
-        map.put("content" , "时间过得好快啊");
-        map.put("userid" , "1016");
-        map.put("visits" , 100);
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("content", "时间过得好快啊");
+//        map.put("userid", "1016");
+        map.put("visits", 100);
         Document document = new Document(map);
         spit.insertOne(document);
 
